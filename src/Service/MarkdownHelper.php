1e13a1f9c1f189f7f35a3e03ec7acab12cc3b9aa
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2019-11-20 16:52:18 +0300

    parameters

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -20,18 +20,27 @@ class MarkdownHelper
 
     private $logger;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown,LoggerInterface  $markdownLogger )
+    private $isDebug;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown,LoggerInterface  $markdownLogger,bool $isDebug)
 
 {
     $this->cache = $cache;
     $this->markdown = $markdown;
     $this->logger =  $markdownLogger;
+    $this->isDebug = $isDebug;
 }
     public function parse(string $source): string
     {   if (stripos($source, 'bacon') !== false) {
 
             $this->logger->info('They are talking about bacon again!');
             }
+
+
+        if ($this->isDebug) {
+            return $this->markdown->transform($source);
+             }
+
         $item=$this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
